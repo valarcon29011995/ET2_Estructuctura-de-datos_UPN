6cbacbc47b56c5f84e6c60fb7c0e43bf58019be9
valarcon29011995 <92767134+valarcon29011995@users.noreply.github.com>
2021-11-07 13:43:22 -0500

    Add files via upload

---
+++ ET2/Pregunta01/Lista.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta01
+{
+    internal class Lista
+    {
+        Stack<Nodo> Listaprogramas = new Stack<Nodo>();
+
+        public void AgregarPrograma(Nodo x)
+        {
+            Listaprogramas.Push(x);
+        }
+
+        public void ImprimirProgramas()
+        {
+            foreach (var Nodo in Listaprogramas)
+            {
+                Console.WriteLine(Nodo.ToString());
+            }
+        }
+
+        public void Eliminar()
+        {
+            Listaprogramas.Pop();
+        }
+    }
+}
---
+++ ET2/Pregunta01/Nodo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta01
+{
+    internal class Nodo
+    {
+        public string NOMBRE, PROGRAMA;
+        public Nodo(string NOMBRE, string PROGRAMA)
+        {
+            this.NOMBRE = NOMBRE;
+            this.PROGRAMA = PROGRAMA;
+        }
+        public override string ToString()
+        {
+            return $"NOMBRE: {this.NOMBRE}\nPROGRAMA: {this.PROGRAMA}\n";
+        }
+    }
+}
