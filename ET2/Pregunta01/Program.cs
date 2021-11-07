264664f444dd4229ccadb8c08d559fa1ef2a0553
valarcon29011995 <92767134+valarcon29011995@users.noreply.github.com>
2021-11-07 17:49:57 -0500

    Add files via upload

---
+++ ET2/Pregunta01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta01
+{
+    internal class Program
+    {
+        static Lista Lprogram = new Lista();
+        static void Main(string[] args)
+        {
+            Lprogram.AgregarPrograma(new Nodo("Programa 1", "Suma"));
+            Lprogram.AgregarPrograma(new Nodo("Programa 2", "Resta"));
+            Lprogram.AgregarPrograma(new Nodo("Programa 3", "Multiplicación"));
+            Lprogram.AgregarPrograma(new Nodo("Programa 4", "División"));
+            Lprogram.AgregarPrograma(new Nodo("Programa 5", "Suma"));
+            Console.WriteLine("+++++++++++++++Lista de Programas++++++++++++++++++");
+            Lprogram.ImprimirProgramas();
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++");             
+            Console.WriteLine("++++++++++Eliminar 2 Ultimos ingresos++++++++++++++");
+            Lprogram.Eliminar();
+            Lprogram.Eliminar();
+            Console.WriteLine("+++++++++++++++Lista de Programas++++++++++++++++++");
+            Lprogram.ImprimirProgramas();
+            Console.Read();
+        }
+        
+    }
+}
